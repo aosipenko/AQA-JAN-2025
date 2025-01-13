c604685f4673935c8a62d6ad7f58a123174479a7
aosipenko <aoskiev@gmail.com>
2025-01-13 19:58:52 +0100

    Added solution

---
+++ src/main/java/org/prog/Car.java
@@ -0,0 +1,13 @@
+package org.prog;
+
+//TODO: add destination parameter to method goTo
+
+public class Car {
+
+    public String color;
+    public String destination;
+
+    public void goTo() {
+        System.out.println(color + " car is driving somewhere " + destination);
+    }
+}
