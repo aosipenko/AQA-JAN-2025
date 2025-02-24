be60d5bd1a5ab987c2b152c89e6c29dbfbd31c03
Natali11111 <getalovatt2@gmail.com>
2025-02-24 18:37:40 +0200

    Updated test

--- src/test/java/AutomationTests.java
+++ src/test/java/AutomationTests.java
@@ -2,21 +2,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class AutomationTests {
-    WebDriver driver;
+    //WebDriver driver;
+    RemoteWebDriver remoteWebDriver;
 
     @Test
     public void testForAllo() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://allo.ua/");
-        List<WebElement> logos = driver.findElements(By.className("v-logo__img"));
+        //driver = new ChromeDriver();
+        remoteWebDriver = new ChromeDriver();
+        remoteWebDriver.manage().window().maximize();
+        remoteWebDriver.get("https://allo.ua/");
+        List<WebElement> logos = remoteWebDriver.findElements(By.className("v-logo__img"));
         Assert.assertFalse(logos.isEmpty());
-        driver.quit();
+        remoteWebDriver.quit();
     }
 }
