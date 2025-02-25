0033a54e4d02970bc2d8f1979e69023bba4cb1da
Natali11111 <getalovatt2@gmail.com>
2025-02-25 12:52:16 +0200

    Added remote web driver

--- src/test/java/AutomationTests.java
+++ src/test/java/AutomationTests.java
@@ -2,24 +2,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 public class AutomationTests {
-    //WebDriver driver;
-    RemoteWebDriver remoteWebDriver;
+    public WebDriver getRemoteDriver() throws MalformedURLException {
+        return new RemoteWebDriver(
+                new URL("http://selenoid-selenoid-1:4444/wd/hub"), new ChromeOptions());
+    }
 
     @Test
-    public void testForAllo() {
-        //driver = new ChromeDriver();
-        remoteWebDriver = new ChromeDriver();
-        remoteWebDriver.manage().window().maximize();
-        remoteWebDriver.get("https://allo.ua/");
-        List<WebElement> logos = remoteWebDriver.findElements(By.className("v-logo__img"));
+    public void testForAllo() throws MalformedURLException {
+        getRemoteDriver().manage().window().maximize();
+        getRemoteDriver().get("https://allo.ua/");
+        List<WebElement> logos = getRemoteDriver().findElements(By.className("v-logo__img"));
         Assert.assertFalse(logos.isEmpty());
-        remoteWebDriver.quit();
+        getRemoteDriver().quit();
     }
+
 }
